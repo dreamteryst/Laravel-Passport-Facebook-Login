b6c21e35288f62badbe806d29f84a65d138eeb23
Kittisak Pimnonthong <dreamt451@gmail.com>
2018-08-04 14:34:33 +0700

    add picture field

--- src/config/facebook.php
+++ src/config/facebook.php
@@ -32,6 +32,7 @@ return [
         'first_name'  => env('FIRST_NAME_COLUMN', 'first_name'),
         'last_name'   => env('LAST_NAME_COLUMN', 'last_name'),
         'name'        => env('NAME_COLUMN', 'name'),
+        'picture'     => env('PICTURE_COLUMN', 'picture'),
         'attach_role' => env('ATTACH_ROLE', null),
     ],
 ];
